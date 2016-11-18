baf6d0f025802b26c437ca13f26ef369c227b498
Jason LeMoine <github@lemoine.io>
2016-11-18 13:48:05 -0500

    Most of functions passing. Two more.

--- app/functions.js
+++ app/functions.js
@@ -2,31 +2,55 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    return fn.apply(null, arr);
   },
 
   speak: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction: function(str) {
-
+    return function (arg) {
+      return str + ', ' + arg;
+    };
   },
 
   makeClosures: function(arr, fn) {
+    var newArray = [];
 
+    var innerFunc = function (arg) {
+      return function () {
+        return fn(arg);
+      };
+    };
+
+    for (var i = 0; i < arr.length; i++) {
+      newArray.push(innerFunc(arr[i]));
+    }
+
+    return newArray;
   },
 
   partial: function(fn, str1, str2) {
-
+    return function (arg) {
+      return fn(str1, str2, arg);
+    };
   },
 
   useArguments: function() {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
 
+    return sum;
   },
 
   callIt: function(fn) {
-
+    // arguments isn't really an array, so we need to create
+    // an array based off a slice of the arguments just dropping the first arg.
+    var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+    return fn.apply(null, args);
   },
 
   partialUsingArguments: function(fn) {
