86e4fe4e49c087e7629014fe80e33501b80d5cd0
Jason LeMoine <github@lemoine.io>
2016-11-19 16:03:40 -0500

    Async passing

--- app/async.js
+++ app/async.js
@@ -2,10 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    // Using native promises because support is there
+    // for modern browsers, and jQuery deferreds are NOT promises.
+    return new Promise(function (resolve, reject) {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
+    return new Promise(function (resolve, reject) {
+      $.getJSON(url).then(function (response) {
+        var peeps = response.people.map(function (person) {
+          return person.name;
+        });
 
+        resolve(peeps.sort());
+      });
+    });
   }
 };
