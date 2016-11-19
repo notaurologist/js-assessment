37ab670292685baa97c6e1635abb345ffed79d08
Jason LeMoine <github@lemoine.io>
2016-11-19 16:25:39 -0500

    Started count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,10 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    return {
+      cancel: function () {
+        clearInterval(counterId);
+      }
+    };
   }
 };
