b258ef5260547fa6f0b78ab3a7f478049ef0ca8e
Jason LeMoine <github@lemoine.io>
2016-11-20 10:16:26 -0500

    Count passing

--- app/count.js
+++ app/count.js
@@ -2,9 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    // No side-effects
+    var i = start;
+    var counterId;
+
+    var logNum = function () {
+      if (i <= end) {
+        console.log(i);
+        i++;
+        counterId = setTimeout(logNum, 100);
+      }
+    };
+
+    logNum();
+
     return {
       cancel: function () {
-        clearInterval(counterId);
+        clearTimeout(counterId);
       }
     };
   }
