53a8fba78b6646dd0a5b9d4213f50403d6d8bdec
Jason LeMoine <github@lemoine.io>
2016-12-02 11:04:14 -0500

    Reverse string

--- app/strings.js
+++ app/strings.js
@@ -10,6 +10,11 @@ exports.stringsAnswers = {
   },
 
   reverseString: function(str) {
+    var revStr = '';
+    for (var i = str.length - 1; i >= 0; i--) {
+      revStr += str[i];
+    }
 
+    return revStr;
   }
 };
